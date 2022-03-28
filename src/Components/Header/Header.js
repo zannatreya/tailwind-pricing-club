7cafdbc4db0b6b4b392154164b756dd511c24c2c
zannatreya <zannatulbintabahar@yahoo.com>
2022-03-28 23:28:39 +0600

    updated

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+
+            <h3 className='text-3xl font-bold underline'>save most of your money</h3>
+
+        </div>
+    );
+};
+
+export default Header;
